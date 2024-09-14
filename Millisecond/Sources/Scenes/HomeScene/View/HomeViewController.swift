a57ed7435db00971f9f5e540b49487a2db2ba457
RAFA <rafa71802937@gmail.com>
2024-09-15 04:01:02 +0900

    feat: 화면 이동 구현

---
+++ Millisecond/Sources/Scenes/HomeScene/View/HomeViewController.swift
@@ -0,0 +1,93 @@
+//
+//  HomeViewController.swift
+//  Millisecond
+//
+//  Created by RAFA on 9/13/24.
+//
+
+import UIKit
+
+import RxRelay
+
+final class HomeViewController: BaseViewController {
+
+    // MARK: - Properties
+
+    private let startButton = UIButton(type: .system)
+    private let viewModel: HomeViewModel
+
+    // MARK: - Initializer
+
+    init(viewModel: HomeViewModel = HomeViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bind()
+    }
+
+    // MARK: - Bindings
+
+    private func bind() {
+        startButton.rx.tap
+            .bind(to: viewModel.input.startButtonTapped)
+            .disposed(by: disposeBag)
+
+        viewModel.output.navigateToGameVC
+            .drive(onNext: { [weak self] in
+                self?.navigateToGameVC()
+            }).disposed(by: disposeBag)
+    }
+
+    // MARK: - Helpers
+
+    private func navigateToGameVC() {
+        let gameVC = GameViewController()
+        let navigationController = UINavigationController(rootViewController: gameVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
+    }
+
+    // MARK: - UI
+
+    override func setupUI() {
+        view.backgroundColor = .black
+
+        startButton.do {
+            $0.backgroundColor = .white
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 10
+            $0.setAttributedTitle(
+                NSAttributedString(
+                    string: "시작하기",
+                    attributes: [
+                        .font: UIFont.systemFont(ofSize: 17, weight: .regular),
+                        .foregroundColor: UIColor.black
+                    ]
+                ),
+                for: .normal
+            )
+        }
+    }
+
+    override func setupSubviews() {
+        view.addSubview(startButton)
+    }
+
+    override func setConstraints() {
+        startButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.left.equalTo(16)
+            $0.height.equalTo(50)
+        }
+    }
+}
