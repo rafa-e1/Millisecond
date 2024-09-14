5635180e8ac5557d6138b401e44deb22493412e5
RAFA <rafa71802937@gmail.com>
2024-09-15 04:01:58 +0900

    feat: 게임 가이드 문구 열거형 구현

---
+++ Millisecond/Sources/Scenes/GameScene/Enum/GameGuideText.swift
@@ -0,0 +1,13 @@
+//
+//  GameGuideText.swift
+//  Millisecond
+//
+//  Created by RAFA on 9/15/24.
+//
+
+import Foundation
+
+enum GameGuideText: String {
+    case startPrompt = "초록색 화면이 나타나면 화면을 눌러주세요."
+    case restartPrompt = "화면을 탭하면 재시작할 수 있습니다."
+}
