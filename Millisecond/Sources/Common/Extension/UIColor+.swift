bd43ecd790a7b9565f5fbb47e93c896a445206b8
RAFA <rafa71802937@gmail.com>
2024-09-24 21:13:45 +0900

    add: 배경색 추가

---
+++ Millisecond/Sources/Common/Extension/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  Millisecond
+//
+//  Created by RAFA on 9/24/24.
+//
+
+import UIKit
+
+extension UIColor {
+
+    static let backgroundColor = #colorLiteral(red: 0.1243003979, green: 0.1250453889, blue: 0.1463056803, alpha: 1)
+}
