130e424e511a79a8c1edf575f5d9060e4f684666
RAFA <rafa71802937@gmail.com>
2024-09-24 13:43:12 +0900

    add: GameState에 가이드 텍스트 추가

--- Millisecond/Sources/Scenes/GameScene/Enum/GameState.swift
+++ Millisecond/Sources/Scenes/GameScene/Enum/GameState.swift
@@ -9,4 +9,12 @@ import Foundation
 
 enum GameState {
     case red, orange, green, result
+
+    var guideText: String {
+        switch self {
+        case .red: return "초록색 화면이 나타나면 화면을 눌러주세요."
+        case .orange: return "초록색 화면이 나타나면 화면을 눌러주세요.\n\n화면을 탭하면 재시작할 수 있습니다."
+        case .green, .result: return "화면을 탭하면 재시작할 수 있습니다."
+        }
+    }
 }
