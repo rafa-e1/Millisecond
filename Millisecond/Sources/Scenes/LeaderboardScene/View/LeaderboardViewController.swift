25fd3cd6e99c7d376ce1617cebf1d6b87b016848
RAFA <rafa71802937@gmail.com>
2024-09-28 21:34:37 +0900

    add: 리더보드 뷰 추가

---
+++ Millisecond/Sources/Scenes/LeaderboardScene/View/LeaderboardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LeaderboardViewController.swift
+//  Millisecond
+//
+//  Created by RAFA on 9/28/24.
+//
+
+import UIKit
+
+final class LeaderboardViewController: BaseViewController {
+
+    override func setupUI() {
+        view.backgroundColor = .backgroundColor
+    }
+}
