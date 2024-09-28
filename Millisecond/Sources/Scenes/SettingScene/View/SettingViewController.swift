270de42f8ffbe721b64a3020e9246f8dd41f105b
RAFA <rafa71802937@gmail.com>
2024-09-28 21:35:06 +0900

    add: 설정 뷰 추가

---
+++ Millisecond/Sources/Scenes/SettingScene/View/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  Millisecond
+//
+//  Created by RAFA on 9/28/24.
+//
+
+import UIKit
+
+final class SettingViewController: BaseViewController {
+
+    override func setupUI() {
+        view.backgroundColor = .backgroundColor
+    }
+}
