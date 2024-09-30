76de5e44e073e6ec92c15ba496025b3424ea08c0
RAFA <rafa71802937@gmail.com>
2024-10-01 01:20:37 +0900

    feat: 로그인 UI 구현

--- Millisecond/Sources/Scenes/LoginScene/View/LoginViewController.swift
+++ Millisecond/Sources/Scenes/LoginScene/View/LoginViewController.swift
@@ -8,31 +8,67 @@
 import AuthenticationServices
 import UIKit
 
+import Lottie
+
 final class LoginViewController: BaseViewController {
 
     private let welcomeLabel = UILabel()
-    private let loginButton = ASAuthorizationAppleIDButton(type: .continue, style: .white)
+    private let thunderAnimationView = LottieAnimationView(name: "thunder")
+    private let timerAnimationView = LottieAnimationView(name: "timer")
+    private let appleSignInButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
 
     override func setupUI() {
         view.backgroundColor = .backgroundColor
 
         welcomeLabel.do {
-            $0.text = "Welcome to Millisecond"
-            $0.font = .systemFont(ofSize: 40, weight: .heavy)
+            $0.text = "Welcome"
+            $0.font = .systemFont(ofSize: 70, weight: .heavy)
             $0.textColor = .white
             $0.textAlignment = .center
+        }
+
+        thunderAnimationView.do {
+            $0.contentMode = .scaleAspectFit
+            $0.loopMode = .loop
+            $0.play()
+        }
+
+        timerAnimationView.do {
+            $0.contentMode = .scaleAspectFit
+            $0.loopMode = .loop
+            $0.play()
         }
     }
 
     override func setupSubviews() {
-        view.addSubview(loginButton)
+        [welcomeLabel, thunderAnimationView, timerAnimationView, appleSignInButton].forEach {
+            view.addSubview($0)
+        }
     }
 
     override func setConstraints() {
-        loginButton.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        welcomeLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
             $0.left.equalTo(16)
-            $0.height.equalTo(50)
+        }
+
+        thunderAnimationView.snp.makeConstraints {
+            $0.centerX.equalToSuperview().dividedBy(1.6)
+            $0.centerY.equalToSuperview().dividedBy(1.1)
+            $0.size.equalTo(300)
+        }
+
+        timerAnimationView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(250)
+        }
+
+        appleSignInButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-40)
+            $0.width.equalTo(343)
+            $0.height.equalTo(56)
         }
     }
 }
