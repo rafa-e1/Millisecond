7f8d9dfb635fa4fbb53be0c8d481298635260579
RAFA <rafa71802937@gmail.com>
2024-10-03 16:47:23 +0900

    refactor: 코드 구조 개선 및 리팩토링

--- Millisecond/Sources/Scenes/LoginScene/ViewModel/LoginIntroViewModel.swift
+++ Millisecond/Sources/Scenes/LoginScene/ViewModel/LoginIntroViewModel.swift
@@ -5,19 +5,20 @@
 //  Created by RAFA on 10/2/24.
 //
 
+import RxCocoa
 import RxSwift
 
-final class LoginIntroViewModel: NSObject {
+final class LoginIntroViewModel {
 
     // MARK: - Input/Output
 
     struct Input {
-        let emailLoginTapped: PublishSubject<Void>
+        let emailLoginTapped: PublishRelay<Void>
     }
 
     struct Output {
-        let signInSuccess: PublishSubject<Bool>
-        let errorMessage: PublishSubject<String>
+        let signInSuccess: Driver<Bool>
+        let errorMessage: Driver<String>
     }
 
     // MARK: - Properties
@@ -25,19 +26,19 @@ final class LoginIntroViewModel: NSObject {
     let input: Input
     let output: Output
 
+    private let signInSuccessRelay = PublishRelay<Bool>()
+    private let errorMessageRelay = PublishRelay<String>()
     private let disposeBag = DisposeBag()
 
     // MARK: - Initializer
 
-    override init() {
-        let emailLoginTapped = PublishSubject<Void>()
-        let signInSuccess = PublishSubject<Bool>()
-        let errorMessage = PublishSubject<String>()
+    init() {
+        let emailLoginTapped = PublishRelay<Void>()
+        let signInSuccess = signInSuccessRelay.asDriver(onErrorJustReturn: false)
+        let errorMessage = errorMessageRelay.asDriver(onErrorJustReturn: "An error occurred.")
 
         self.input = Input(emailLoginTapped: emailLoginTapped)
         self.output = Output(signInSuccess: signInSuccess, errorMessage: errorMessage)
-
-        super.init()
 
         bindInput()
     }
@@ -47,8 +48,18 @@ final class LoginIntroViewModel: NSObject {
     private func bindInput() {
         input.emailLoginTapped
             .subscribe(onNext: { [weak self] in
-                
+                self?.performEmailLogin()
             })
             .disposed(by: disposeBag)
+    }
+
+    private func performEmailLogin() {
+        let loginSuccess = true
+
+        if loginSuccess {
+            signInSuccessRelay.accept(true)
+        } else {
+            errorMessageRelay.accept("Login failed. Please try again.")
+        }
     }
 }
