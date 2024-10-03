7a76d059add7e1cde288696b7405ae9c3c324c9d
RAFA <rafa71802937@gmail.com>
2024-10-03 17:22:19 +0900

    add: 로그인 뷰 생성

---
+++ Millisecond/Sources/Scenes/LoginScene/View/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Millisecond
+//
+//  Created by RAFA on 10/3/24.
+//
+
+import UIKit
+
+final class LoginViewController: BaseViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    override func setupUI() {
+        view.backgroundColor = .backgroundColor
+    }
+}
