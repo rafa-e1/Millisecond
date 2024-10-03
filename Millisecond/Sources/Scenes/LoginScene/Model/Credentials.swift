2d0c400350c47cc8f7a1aa79fffc7b2587e1ff43
RAFA <rafa71802937@gmail.com>
2024-10-03 17:26:40 +0900

    add: 사용자 인증을 위한 Credentials 구조체 추가

---
+++ Millisecond/Sources/Scenes/LoginScene/Model/Credentials.swift
@@ -0,0 +1,18 @@
+//
+//  Credentials.swift
+//  Millisecond
+//
+//  Created by RAFA on 10/2/24.
+//
+
+import UIKit
+
+import FirebaseAuth
+import FirebaseFirestore
+
+struct Credentials {
+    let email: String
+    let password: String
+    let nickname: String
+    let profileImage: UIImage
+}
