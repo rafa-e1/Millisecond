87fe4d3e34171b109d5c89b0669bbe5139138d41
RAFA <rafa71802937@gmail.com>
2024-10-03 17:20:07 +0900

    refactor: 불필요한 코드 제거

--- Millisecond/Sources/Scenes/LoginScene/ViewModel/LoginIntroViewModel.swift
+++ Millisecond/Sources/Scenes/LoginScene/ViewModel/LoginIntroViewModel.swift
@@ -18,7 +18,6 @@ final class LoginIntroViewModel {
 
     struct Output {
         let signInSuccess: Driver<Bool>
-        let errorMessage: Driver<String>
     }
 
     // MARK: - Properties
@@ -26,8 +25,6 @@ final class LoginIntroViewModel {
     let input: Input
     let output: Output
 
-    private let signInSuccessRelay = PublishRelay<Bool>()
-    private let errorMessageRelay = PublishRelay<String>()
     private let disposeBag = DisposeBag()
 
     // MARK: - Initializer
@@ -35,31 +32,8 @@ final class LoginIntroViewModel {
     init() {
         let emailLoginTapped = PublishRelay<Void>()
         let signInSuccess = signInSuccessRelay.asDriver(onErrorJustReturn: false)
-        let errorMessage = errorMessageRelay.asDriver(onErrorJustReturn: "An error occurred.")
 
         self.input = Input(emailLoginTapped: emailLoginTapped)
         self.output = Output(signInSuccess: signInSuccess, errorMessage: errorMessage)
-
-        bindInput()
-    }
-
-    // MARK: - Helpers
-
-    private func bindInput() {
-        input.emailLoginTapped
-            .subscribe(onNext: { [weak self] in
-                self?.performEmailLogin()
-            })
-            .disposed(by: disposeBag)
-    }
-
-    private func performEmailLogin() {
-        let loginSuccess = true
-
-        if loginSuccess {
-            signInSuccessRelay.accept(true)
-        } else {
-            errorMessageRelay.accept("Login failed. Please try again.")
-        }
     }
 }
