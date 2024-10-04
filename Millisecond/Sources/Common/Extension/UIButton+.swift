0d96a1e39f2384deecd81d2ed9126b95735cb9a5
RAFA <rafa71802937@gmail.com>
2024-10-05 08:09:31 +0900

    add: 버튼 커스텀 타이틀 추가

---
+++ Millisecond/Sources/Common/Extension/UIButton+.swift
@@ -0,0 +1,36 @@
+//
+//  UIButton+.swift
+//  Millisecond
+//
+//  Created by RAFA on 10/5/24.
+//
+
+import UIKit
+
+extension UIButton {
+
+    func attributedTitle(
+        firstPart: String,
+        _ firstPartColor: UIColor,
+        secondPart: String,
+        _ secondPartColor: UIColor
+    ) {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 16, weight: .regular),
+            .foregroundColor: firstPartColor
+        ]
+
+        let attributedTitle = NSMutableAttributedString(
+            string: "\(firstPart)",
+            attributes: attributes
+        )
+
+        let boldString: [NSAttributedString.Key: Any] = [
+            .font: UIFont.boldSystemFont(ofSize: 16),
+            .foregroundColor: secondPartColor
+        ]
+
+        attributedTitle.append(NSAttributedString(string: secondPart, attributes: boldString))
+        setAttributedTitle(attributedTitle, for: .normal)
+    }
+}
